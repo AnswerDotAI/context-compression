859bd93375930a95e8ad06f981cb65a0143ad232
Griffin Adams <ga@answer.ai>
2024-06-07 19:23:57 +0000

    Add QuALITY dataset.

--- data_utils.py
+++ data_utils.py
@@ -1,5 +1,9 @@
+import os
+import string
 from abc import ABC, abstractmethod
 from datasets import load_dataset, Dataset, DatasetDict
+from tqdm import tqdm
+import json
 
 
 class Benchmark(ABC):
@@ -20,9 +24,11 @@ class Benchmark(ABC):
     def get_validation(self) -> Dataset:
         return self.dataset["validation"]
 
-    @abstractmethod
+    def id(self, ex: dict) -> str:
+        return ex["id"]
+
     def question(self, ex: dict) -> str:
-        pass
+        return ex["question"]
 
     @abstractmethod
     def instruction(self, ex: dict) -> str:
@@ -30,11 +36,11 @@ class Benchmark(ABC):
 
     @abstractmethod
     def context(self, ex: dict) -> str:
-        pass
+        return ex["context"]
 
     @abstractmethod
-    def answer(self, ex: dict) -> str:
-        pass
+    def answer(self, ex: dict) -> list[str]:
+        return ex["answer"]
 
 
 class TriviaQA(Benchmark):
@@ -47,6 +53,9 @@ class TriviaQA(Benchmark):
 
     def question(self, ex: dict) -> str:
         return ex["question"]
+
+    def id(self, ex: dict) -> str:
+        return ex["question_id"]
 
     def instruction(self) -> str:
         return "Answer the following question."
@@ -88,10 +97,98 @@ class TriviaQA(Benchmark):
         return ex["answer"]["aliases"]
 
 
-BENCHMARKS = {"triviaqa": TriviaQA}
+class Quality(Benchmark):
+    DATA_URL = (
+        "https://github.com/nyu-mll/quality/blob/main/data/v1.0.1/QuALITY.v1.0.1.zip"
+    )
+    VERSION = "QuALITY.v1.0.1.htmlstripped"
+
+    def __init__(self, cache_dir: str = "/workspace/.cache/datasets"):
+        os.makedirs(cache_dir, exist_ok=True)
+        self.cache_dir = cache_dir
+        super().__init__("quality")
+
+    def dataset_from_fn(self, fn):
+        # Load JSONlines file into list of dictionaries
+        with open(fn, "r") as fd:
+            lines = list(tqdm(map(json.loads, fd)))
+
+        dataset = []
+        for line in lines:
+            line["article"] = line["article"].strip()
+            line["year"] = (
+                int(line["year"]) if line["year"] is not None else None
+            )  # Inconsistent type in the dataset
+            questions = line.pop("questions")
+
+            for q in questions:
+                options = q["options"]
+                qid = q["question_unique_id"]
+                label = q["gold_label"] - 1 if "gold_label" in q else None
+                answer = options[label] if label is not None else None
+
+                dataset.append(
+                    {
+                        "id": qid,
+                        "question": q["question"],
+                        "label": label,
+                        "answer": answer,
+                        "options": q["options"],
+                        "difficult": q["difficult"],
+                        **line,
+                    }
+                )
+
+        print(f"Processed {len(dataset)} examples for {fn}.")
+        return Dataset.from_list(dataset)
+
+    def download(self) -> Dataset:
+        # Path to save the downloaded zip file
+        data_dir = os.path.join(self.cache_dir, "QuALITY.v1.0.1")
+        if not os.path.exists(data_dir):
+            raise FileNotFoundError(
+                f"Download the data from {Quality.DATA_URL} and unzip the directory to location {data_dir}."
+            )
+
+        return DatasetDict(
+            {
+                "train": self.dataset_from_fn(
+                    os.path.join(data_dir, f"{Quality.VERSION}.train")
+                ),
+                "validation": self.dataset_from_fn(
+                    os.path.join(data_dir, f"{Quality.VERSION}.dev")
+                ),
+                "test": self.dataset_from_fn(
+                    os.path.join(data_dir, f"{Quality.VERSION}.test")
+                ),
+            }
+        )
+
+    def question(self, ex: dict) -> str:
+        q = ex["question"]
+        options = "\n".join(
+            [f"{string.ascii_uppercase[i]}) {o}" for i, o in enumerate(ex["options"])]
+        )
+        return f"{q}\n\n# Options\n{options}"
+
+    def instruction(self) -> str:
+        return "Which of the given options best answers the question? Return the uppercase letter corresponding to the correct option."
+
+    def context(self, ex: dict) -> str:
+        return ex["article"]
+
+    def answer(self, ex: dict) -> str | list[str]:
+        return string.ascii_uppercase[ex["label"]]
+
+
+BENCHMARKS = {"triviaqa": TriviaQA, "quality": Quality}
 
 
 if __name__ == "__main__":
-    dataset = TriviaQA(use_web=False)
+    dataset = BENCHMARKS["quality"]()
 
-    print(dataset.answer(dataset.get_train()[0]))
+    ex = dataset.get_train()[0]
+
+    print(dataset.context(ex))
+    print(dataset.question(ex))
+    print(dataset.answer(ex))
